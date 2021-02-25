8af4c098e8f1ae1f23d0bc56493b39e5bd1a0826
amaniago <anthony.maniago@bleu122.com>
2021-02-25 23:42:17 +0100

    Version pair/Impair sur 2 dés

--- app/src/main/java/com/thomas/MainActivity.java
+++ app/src/main/java/com/thomas/MainActivity.java
@@ -12,38 +12,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        relancerDes(null);
+//        relancerDes(null);
+        calculPairImpair(null);
     }
 
-    public void relancerDes(View view) {
-        int compteur6 = 0;
-        int compteur7 = 0;
-        int compteur8 = 0;
+    public void calculPairImpair(View view) {
+        int de_1 = (int) (Math.random() * (6 - 1)) + 1;
+        int de_2 = (int) (Math.random() * (6 - 1)) + 1;
 
-        int de_1 = 1;
-        int de_2 = 1;
+        int somme = de_1 + de_2;
 
-        for (int i = 0; i < 60000; i++) {
-            de_1 = (int) (Math.random()*(6-1)) + 1;
-            de_2 = (int) (Math.random()*(6-1)) + 1;
+        findViewById(R.id.compteur_7).setVisibility(View.GONE);
+        findViewById(R.id.compteur_8).setVisibility(View.GONE);
 
-            switch (de_1 + de_2) {
-                case 6 :
-                    compteur6++;
-                    break;
-                case 7:
-                    compteur7++;
-                    break;
-                case 8:
-                    compteur8++;
-                    break;
-            }
-
+        if(somme % 2 == 1) {
+            ((TextView) findViewById(R.id.compteur_6)).setText("Nombre impair (" + somme + ")");
+        } else {
+            ((TextView) findViewById(R.id.compteur_6)).setText("Nombre pair (" + somme + ")");
         }
 
-        ((TextView) findViewById(R.id.compteur_6)).setText("Nombre de 6 : " + compteur6);
-        ((TextView) findViewById(R.id.compteur_7)).setText("Nombre de 7 : " + compteur7);
-        ((TextView) findViewById(R.id.compteur_8)).setText("Nombre de 8 : " + compteur8);
     }
+
+//    public void relancerDes(View view) {
+//        int compteur6 = 0;
+//        int compteur7 = 0;
+//        int compteur8 = 0;
+//
+//        int de_1 = 1;
+//        int de_2 = 1;
+//
+//        for (int i = 0; i < 60000; i++) {
+//            de_1 = (int) (Math.random()*(6-1)) + 1;
+//            de_2 = (int) (Math.random()*(6-1)) + 1;
+//
+//            switch (de_1 + de_2) {
+//                case 6 :
+//                    compteur6++;
+//                    break;
+//                case 7:
+//                    compteur7++;
+//                    break;
+//                case 8:
+//                    compteur8++;
+//                    break;
+//            }
+//        }
+//
+//        ((TextView) findViewById(R.id.compteur_6)).setText("Nombre de 6 : " + compteur6);
+//        ((TextView) findViewById(R.id.compteur_7)).setText("Nombre de 7 : " + compteur7);
+//        ((TextView) findViewById(R.id.compteur_8)).setText("Nombre de 8 : " + compteur8);
+//    }
 }
