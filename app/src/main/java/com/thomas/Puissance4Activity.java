f6626fdd5b2a2b3c3ad57c4755a2fe26fa3e5a9b
amaniago <anthony.maniago@bleu122.com>
2021-06-24 22:40:42 +0200

    Création puissance 4

--- app/src/main/java/com/thomas/Puissance4Activity.java
+++ app/src/main/java/com/thomas/Puissance4Activity.java
@@ -1,17 +1,20 @@
 package com.thomas;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 
 public class Puissance4Activity extends AppCompatActivity {
-    String Joueur = "yellow";
+    int joueur = R.color.yellow;
 
     int nbTour = 0;
     int colonne1 = 0;
@@ -30,55 +33,50 @@ public class Puissance4Activity extends AppCompatActivity {
         setContentView(R.layout.activity_puissance_4);
 
         findViewById(R.id.column1).setOnClickListener(view -> {
-            clickColonne("1");
+            clickColonne(1, view);
         });
 
         findViewById(R.id.column2).setOnClickListener(view -> {
-            clickColonne("2");
+            clickColonne(2, view);
         });
 
         findViewById(R.id.column3).setOnClickListener(view -> {
-            clickColonne("3");
+            clickColonne(3, view);
         });
 
         findViewById(R.id.column4).setOnClickListener(view -> {
-            clickColonne("4");
+            clickColonne(4, view);
         });
 
         findViewById(R.id.column5).setOnClickListener(view -> {
-            clickColonne("5");
+            clickColonne(5, view);
         });
 
         findViewById(R.id.column6).setOnClickListener(view -> {
-            clickColonne("6");
+            clickColonne(6, view);
         });
 
         findViewById(R.id.column7).setOnClickListener(view -> {
-            clickColonne("7");
+            clickColonne(7, view);
         });
     }
 
-    public void clickColonne(String colonne) {
+    public void clickColonne(int colonneNumber, View colonneView) {
         nbTour++;
 
-        if (colonne.equals("1")) {
+        if (colonneNumber == 1) {
             if (colonne1 < 6) {
-
-                //mettre le pion en n+1
                 colonne1++;
+                int caseId = getResources().getIdentifier("case" + colonneNumber, "id", getApplicationContext().getPackageName());
+                ImageView caseView = colonneView.findViewById(caseId);
+                caseView.setColorFilter(ContextCompat.getColor(getApplicationContext(), joueur), android.graphics.PorterDuff.Mode.MULTIPLY);
             }
         }
 
-
-
-
-
-// mettre le pion en fonction du joueur et de la colonne
-// +1 à la colonne récupérée
         if (nbTour % 2 == 0) {
-            Joueur = "yellow";
+            joueur = R.color.yellow;
         } else {
-            Joueur = "red";
+            joueur = R.color.red;
         }
     }
 }
