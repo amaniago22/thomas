112cf63c1a6d89cc0aa78422d75013567bcbd9d9
lospetitos <doody3131@gmail.com>
2021-06-24 22:08:36 +0200

    qdqdqz

--- app/src/main/java/com/thomas/Puissance4Activity.java
+++ app/src/main/java/com/thomas/Puissance4Activity.java
@@ -10,12 +10,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 public class Puissance4Activity extends AppCompatActivity {
+    String Joueur = "yellow";
 
+
+    int nbTour = 0;
+    int colonne1 = 0;
+    int colonne2 = 0;
+    int colonne3 = 0;
+    int colonne4 = 0;
+    int colonne5 = 0;
+    int colonne6 = 0;
+
+    ArrayList<String> casesJoueur1 = new ArrayList<>();
+    ArrayList<String> casesJoueur2 = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puissance_4);
+    }
+
+    public void clickColonne(View view) {
+        nbTour++;
+// récupérer colonne
+
+
+
+
+
+// mettre le pion en fonction du joueur et de la colonne
+// +1 à la colonne récupérée
+        if (nbTour % 2 == 0) {
+            Joueur = "yellow";
+        } else {
+            Joueur = "red";
+        }
     }
 
 }
